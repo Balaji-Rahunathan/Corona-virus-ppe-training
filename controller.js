49e44651b9109032a1760180c34021f8b75ea417
Balaji <mynameisbalag@gmail.com>
2020-02-16 20:34:58 +0530

    Slide function animation added

--- controller.js
+++ controller.js
@@ -35,3 +35,22 @@ $("#rightImage").droppable({
 function reset() {
     document.getElementById("next").style.display = "none";
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+showDivs(slideIndex += n);
+reset()
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideContainer");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "flex";  
+}
