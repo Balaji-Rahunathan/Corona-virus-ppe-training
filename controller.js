3134f8dfd528d8f94397dc2f7bad32fb3017a914
Balaji <mynameisbalag@gmail.com>
2020-02-23 22:50:15 +0530

    questions added

--- controller.js
+++ controller.js
@@ -79,3 +79,125 @@ function reset() {
     }
 }
 
+function toggle(event){
+    var str 
+
+    if(event.target.id.includes("f"))
+    {
+         document.getElementById(event.target.id).className = "true" 
+         str = event.target.id;
+         var value = str.replace("f","t");
+         document.getElementById(value).className = "false" 
+    }
+    else{
+        document.getElementById(event.target.id).className = "true" 
+        str = event.target.id;
+        var value = str.replace("t","f");
+        document.getElementById(value).className = "false" 
+
+    }
+
+    var q1 = document.getElementById("q1t").className
+    var q2 = document.getElementById("q2t").className
+    var q3 = document.getElementById("q3f").className
+
+    if(q1=="true"&&q2=="true"&&q3=="true")
+    {
+        document.getElementById("answer").innerHTML = "Perfect, Let's finish "
+        // loop();
+    }
+    else{
+        document.getElementById("answer").innerHTML = "Not Sure Go back and check again"
+    }
+}
+
+function answer(){
+    var q1 = document.getElementById("q1t").className
+    var q2 = document.getElementById("q2t").className
+    var q3 = document.getElementById("q3f").className
+
+    if(q1=="true"&&q2=="true"&&q3=="true")
+    {
+        document.getElementById("answer").href = "https://skillcord.com/app/signup/course/corono"
+    }
+    else{
+        document.getElementById("answer").onclick = plusDivs(-10)
+    }
+
+    // if()
+    // plusDivs(-10)
+}
+
+
+////confetti animation
+
+const canvasEl = document.querySelector('#canvas');
+
+const w = canvasEl.width = window.innerWidth;
+const h = canvasEl.height = window.innerHeight * 2;
+
+function loop() {
+  requestAnimationFrame(loop);
+	ctx.clearRect(0,0,w,h);
+  
+  confs.forEach((conf) => {
+    conf.update();
+    conf.draw();
+  })
+}
+
+function Confetti () {
+  //construct confetti
+  const colours = ['#fde132', '#009bde', '#ff6b00'];
+  
+  this.x = Math.round(Math.random() * w);
+  this.y = Math.round(Math.random() * h)-(h/2);
+  this.rotation = Math.random()*360;
+
+  const size = Math.random()*(w/60);
+  this.size = size < 15 ? 15 : size;
+
+  this.color = colours[Math.floor(colours.length * Math.random())];
+
+  this.speed = this.size/7;
+  
+  this.opacity = Math.random();
+
+  this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
+}
+
+Confetti.prototype.border = function() {
+  if (this.y >= h) {
+    this.y = h;
+  }
+}
+
+Confetti.prototype.update = function() {
+  this.y += this.speed;
+  
+  if (this.y <= h) {
+    this.x += this.shiftDirection/3;
+    this.rotation += this.shiftDirection*this.speed/100;
+  }
+
+  if (this.y > h) this.border();
+};
+
+Confetti.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
+  ctx.lineTo(this.x, this.y);
+  ctx.closePath();
+  ctx.globalAlpha = this.opacity;
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
+
+const ctx = canvasEl.getContext('2d');
+const confNum = Math.floor(w / 4);
+const confs = new Array(confNum).fill().map(_ => new Confetti());
+
+// loop();
+
+
+console.log(navigator);
